01969395b788219f1b97896fdf6c36674a7bc62b
Carlos Pitre <carlsjulio@gmail.com>
2016-02-09 16:31:58 -0500

    CRUD Cliente

--- Controladores/ClienteControl.php
+++ Controladores/ClienteControl.php
@@ -5,48 +5,96 @@ use Slim\Http\Response;
 class ClienteControl{
 
   function getAll(Request $request, Response $response) {
-      $response = $response->withHeader('Content-type', 'application/json');
-      $data = Cliente::all();
-      if(count($data) == 0){
-        $response = $response->withStatus(404);
-      }
-      $response->getBody()->write($data);
-      return $response;
+    $response = $response->withHeader('Content-type', 'application/json');
+    $data = Cliente::all();
+    if(count($data) == 0){
+      $response = $response->withStatus(404);
+    }
+    $response->getBody()->write($data);
+    return $response;
   }
 
   function getById(Request $request, Response $response){
-      $response = $response->withHeader('Content-type', 'application/json');
-      $id = $request->getAttribute("id");
-      $data = Cliente::select("*")
-                      ->where("id","=",$id)
-                      ->get();
-      $response->getBody()->write($data);
-      return $response;
+    $response = $response->withHeader('Content-type', 'application/json');
+    $id = $request->getAttribute("id");
+    $data = Cliente::select("*")
+                    ->where("id","=",$id)
+                    ->get();
+    $response->getBody()->write($data);
+    return $response;
   }
 
   function post(Request $request, Response $response){
+    $response = $response->withHeader('Content-type', 'application/json');
+    $data = json_decode($request->getBody(),true);
+
+    try{
+        $cliente = new Cliente;
+        $cliente->email     =   $data['email'];
+        $cliente->nombres   =   $data['nombres'];
+        $cliente->apellidos =   $data['apellidos'];
+        $cliente->telefono  =   $data['telefono'];
+        $cliente->pass      =   sha1($data['pass']);
+        $cliente->idPush    =   $data['idPush'];
+        $cliente->idFace    =   $data['idFace'];
+        $cliente->estado    =   "ACTIVO";
+        $cliente->save();
+        $respuesta = json_encode(array('msg' => "Guardado correctamente", "std" => 1));
+        $response = $response->withStatus(200);
+    }catch(Exception $err){
+        $respuesta = json_encode(array('msg' => "error", "std" => 0,"err" => $err->getMessage()));
+        $response = $response->withStatus(404);
+    }
+    $response->getBody()->write($respuesta);
+    return $response;
+  }
+
+  public function delete(Request $request, Response $response)
+  {
+    try {
+      $response = $response->withHeader('Content-type', 'application/json');
+      $id = $request->getAttribute("id");
+      $cliente = Cliente::select("*")
+                      ->where("id","=",$id)
+                      ->get();
+      $cliente->estado = "INACTIVO";
+      $cliente->save();
+      $respuesta = json_encode(array('msg' => "Eliminado correctamente", "std" => 1));
+      $response = $response->withStatus(200);
+    } catch (Exception $err) {
+      $respuesta = json_encode(array('msg' => "error", "std" => 0,"err" => $err->getMessage()));
+      $response = $response->withStatus(404);
+    }
+    $response->getBody()->write($respuesta);
+    return $response;
+      
+  }
+
+  public function update(Request $request, Response $response)
+  {
+    try {
       $response = $response->withHeader('Content-type', 'application/json');
       $data = json_decode($request->getBody(),true);
-
-      try{
-          $cliente = new Cliente;
-          $cliente->email     =   $data['email'];
-          $cliente->nombres   =   $data['nombres'];
-          $cliente->apellidos =   $data['apellidos'];
-          $cliente->telefono  =   $data['telefono'];
-          $cliente->pass      =   sha1($data['pass']);
-          $cliente->idPush    =   $data['idPush'];
-          $cliente->idFace    =   $data['idFace'];
-          $cliente->estado    =   "ACTIVO";
-          $cliente->save();
-          $respuesta = json_encode(array('msg' => "Guardado correctamente", "std" => 1));
-          $response = $response->withStatus(200);
-      }catch(Exception $err){
-          $respuesta = json_encode(array('msg' => "error", "std" => 0,"err" => $err->getMessage()));
-          $response = $response->withStatus(404);
-      }
-      $response->getBody()->write($respuesta);
-      return $response;
+      $id = $request->getAttribute("id");
+      $cliente = Cliente::select("*")
+                          ->where("id","=",$id)
+                          ->get();
+      $cliente->email     =   $data['email'];
+      $cliente->nombres   =   $data['nombres'];
+      $cliente->apellidos =   $data['apellidos'];
+      $cliente->telefono  =   $data['telefono'];
+      $cliente->pass      =   sha1($data['pass']);
+      $cliente->idPush    =   $data['idPush'];
+      $cliente->idFace    =   $data['idFace'];
+      $cliente->save();
+      $respuesta = json_encode(array('msg' => "Eliminado correctamente", "std" => 1));
+      $response = $response->withStatus(200);
+    } catch (Exception $err) {
+      $respuesta = json_encode(array('msg' => "error", "std" => 0,"err" => $err->getMessage()));
+      $response = $response->withStatus(404);
+    }
+    $response->getBody()->write($respuesta);
+    return $response;
   }
 
 }
--- Router/ClienteRouter.php
+++ Router/ClienteRouter.php
@@ -3,3 +3,5 @@
 $app->get('/cliente', "ClienteControl:getAll");
 $app->get('/cliente/{id}', "ClienteControl:getById");
 $app->post('/cliente', "ClienteControl:post");
+$app->delete('/cliente/{id}', "ClienteControl:delete");
+$app->put('/cliente/{id}',"ClienteControl;update");

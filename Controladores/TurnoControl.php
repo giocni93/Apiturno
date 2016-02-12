3d46dda0a91cb7c74e8097b10828dbf0040318fc
giocni93 <giocni@gmail.com>
2016-02-12 17:48:05 -0500

    gio

--- Controladores/TurnoControl.php
+++ Controladores/TurnoControl.php
@@ -4,4 +4,74 @@ use Slim\Http\Response;
 
 class TurnoControl{
 
+  public function getTurnosEnColaByEmpleado(Request $request, Response $response){
+    $response = $response->withHeader('Content-type', 'application/json');
+    $idEmpleado = $request->getAttribute("idEmpleado");
+    $data = Turno::select("turno.id","cliente.nombres","cliente.apellidos","turno.turno","turno.estadoTurno","cliente.idPush")
+                    ->join("cliente","cliente.id","=","turno.idCliente")
+                    ->where("turno.idEmpleado","=",$idEmpleado)
+                    ->where("turno.estadoTurno","=","CONFIRMADO")
+                    ->orwhere("turno.estadoTurno","=","ATENDIENDO")
+                    ->where("turno.estado","=","ACTIVO")
+                    ->whereRaw('ABS(TIMESTAMPDIFF(MINUTE,turno.fechaSolicitud,NOW())) >= turno.tiempo', [])
+                    ->orderBy('turno.id', 'asc')
+                    ->orderBy('turno.tiempo', 'asc')
+                    ->get();
+    if(count($data) == 0){
+      $response = $response->withStatus(404);
+    }
+    $response->getBody()->write($data);
+    return $response;
+  }
+
+  public function getTurnosEnEsperaByEmpleado(Request $request, Response $response){
+    $response = $response->withHeader('Content-type', 'application/json');
+    $idEmpleado = $request->getAttribute("idEmpleado");
+    $data = Turno::select("turno.id","cliente.nombres","cliente.apellidos","turno.turno","turno.estadoTurno","cliente.idPush")
+                    ->join("cliente","cliente.id","=","turno.idCliente")
+                    ->where("turno.idEmpleado","=",$idEmpleado)
+                    ->where("turno.estadoTurno","=","SOLICITADO")
+                    ->where("turno.estado","=","ACTIVO")
+                    ->orderBy('turno.id', 'asc')
+                    ->orderBy('turno.tiempo', 'asc')
+                    ->get();
+    if(count($data) == 0){
+      $response = $response->withStatus(404);
+    }
+    $response->getBody()->write($data);
+    return $response;
+  }
+
+  public function cambiarEstadoTurno(Request $request, Response $response){
+    $response = $response->withHeader('Content-type', 'application/json');
+    $data = json_decode($request->getBody(),true);
+    $id = $request->getAttribute("id");
+    $banTerminado = false;
+    try {
+      $turno = Turno::select("*")
+                      ->where("id","=",$id)
+                      ->first();
+      $turno->estadoTurno   =   $data['estadoTurno'];
+      if($turno->estadoTurno == "TERMINADO"){
+        $turno->fechaFinal = "2016-02-12 15:27:26";
+        $banTerminado = true;
+      }
+      if($turno->estadoTurno == "ATENDIENDO"){
+        $turno->fechaInicio = "2016-02-12 15:27:26";
+      }
+      $turno->save();
+      if($banTerminado){
+        //ENVIAR NOTIFICACIONES A LOS CLIENTES
+        
+      }
+      $respuesta = json_encode(array('msg' => "Modificado correctamente", "std" => 1));
+      $response = $response->withStatus(200);
+    } catch (Exception $err) {
+      $respuesta = json_encode(array('msg' => "error", "std" => 0,"err" => $err->getMessage()));
+      $response = $response->withStatus(404);
+    }
+    $response->getBody()->write($respuesta);
+    return $response;
+  }
+
 }
--- Router/TurnoRouter.php
+++ Router/TurnoRouter.php
@@ -1,1 +1,5 @@
 <?php
+
+$app->get('/empleado/{idEmpleado}/confirmados/turnos', "TurnoControl:getTurnosEnColaByEmpleado");
+$app->get('/empleado/{idEmpleado}/solicitados/turnos', "TurnoControl:getTurnosEnEsperaByEmpleado");
+$app->put('/turno/{id}', "TurnoControl:cambiarEstadoTurno");

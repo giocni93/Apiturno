617d491c29aaace42d5bf721d27ebd69cd136f92
giocni93 <giocni@gmail.com>
2016-02-27 14:13:47 -0500

    nuevo cliente por email

--- Router/ClienteRouter.php
+++ Router/ClienteRouter.php
@@ -2,6 +2,7 @@
 
 $app->get('/cliente', "ClienteControl:getAll");
 $app->get('/cliente/{id}', "ClienteControl:getById");
+$app->get('/email/{email}/cliente', "ClienteControl:getByEmail");
 $app->post('/cliente', "ClienteControl:post");
 $app->delete('/cliente/{id}', "ClienteControl:delete");
 $app->put('/cliente/{id}',"ClienteControl:put");

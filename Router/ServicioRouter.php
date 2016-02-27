9ca67b7751faacc4283e41467477ff7574f5b65b
Carlos Pitre <carlsjulio@gmail.com>
2016-02-27 10:05:44 -0500

    servicios

--- Router/ServicioRouter.php
+++ Router/ServicioRouter.php
@@ -5,3 +5,4 @@ $app->put('/servicio/{id}',"ServicioControl:updateservicios");
 $app->put('/servicio/estado/{id}',"ServicioControl:updateestado");
 $app->put('/servicio/estado/desactivar/{id}',"ServicioControl:updateestadodesactivar");
 $app->get('/sucursal/{idSucursal}/servicio',"ServicioControl:getServiciosBySucursal");
+$app->get('/sector/{idSector}/servicio',"ServicioControl:getServiciosBySector");

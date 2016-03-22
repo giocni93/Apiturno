3a57cb85c9083d8b7108729327a1794150558de0
giocni93 <giocni@gmail.com>
2016-03-22 10:41:48 -0500

    perra

--- Controladores/SectorControl.php
+++ Controladores/SectorControl.php
@@ -7,7 +7,7 @@ class SectorControl{
 
 	function getAll(Request $request, Response $response) {
 	    $response = $response->withHeader('Content-type', 'application/json');
-	    $data = Sector::all();
+	    $data = Sector::select("*")->where("estado","=","ACTIVO")->get();
 	    if(count($data) == 0){
 	      $response = $response->withStatus(404);
 	    }
